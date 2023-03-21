0ec00edf4561bdc5b4c6ec20b38fd1cba1b28887
tigr322 <70834080+tigr322@users.noreply.github.com>
2023-03-21 09:01:52 +0300

    Update form.php

--- Thirdback/form.php
+++ Thirdback/form.php
@@ -1,12 +1,12 @@
 <form action="" method="POST">
-  <input name="fio" />
-  <select name="year">
+ <p> Ваше имя <input name="fio" /> </p>
+  <p> Год <select name="year"> </p>
     <?php 
     for ($i = 1922; $i <= 2022; $i++) {
       printf('<option value="%d">%d год</option>', $i, $i);
     }
     ?>
   </select>
-  <input e-mail ="email" />
+  <p><input e-mail ="email" /> </p>
   <input type="submit" value="ok" />
 </form>
