01708a97879b8957425197d9d9d843868722c4f6
tigr322 <70834080+tigr322@users.noreply.github.com>
2023-04-30 13:40:47 +0300

    Update index.php

--- Fourback/index.php
+++ Fourback/index.php
@@ -1,90 +1,71 @@
 <?php
-/**
- * Реализовать проверку заполнения обязательных полей формы в предыдущей
- * с использованием Cookies, а также заполнение формы по умолчанию ранее
- * введенными значениями.
- */
-
-// Отправляем браузеру правильную кодировку,
-// файл index.php должен быть в кодировке UTF-8 без BOM.
 header('Content-Type: text/html; charset=UTF-8');
 
-// В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
-// и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-  // Массив для временного хранения сообщений пользователю.
-  $messages = array();
-
-  // В суперглобальном массиве $_COOKIE PHP хранит все имена и значения куки текущего запроса.
-  // Выдаем сообщение об успешном сохранении.
-  if (!empty($_COOKIE['save'])) {
-    // Удаляем куку, указывая время устаревания в прошлом.
-    setcookie('save', '', 100000);
-    // Если есть параметр save, то выводим сообщение пользователю.
-    $messages[] = 'Спасибо, результаты сохранены.';
+  if (!empty($_GET['save'])) {
   }
-
-  // Складываем признак ошибок в массив.
-  $errors = array();
-  $errors['fio'] = !empty($_COOKIE['fio_error']);
-  // TODO: аналогично все поля.
-
-  // Выдаем сообщения об ошибках.
-  if ($errors['fio']) {
-    // Удаляем куку, указывая время устаревания в прошлом.
-    setcookie('fio_error', '', 100000);
-    // Выводим сообщение.
-    $messages[] = '<div class="error">Заполните имя.</div>';
-  }
-  // TODO: тут выдать сообщения об ошибках в других полях.
-
-  // Складываем предыдущие значения полей в массив, если есть.
-  $values = array();
-  $values['fio'] = empty($_COOKIE['fio_value']) ? '' : $_COOKIE['fio_value'];
-  // TODO: аналогично все поля.
-
-  // Включаем содержимое файла form.php.
-  // В нем будут доступны переменные $messages, $errors и $values для вывода 
-  // сообщений, полей с ранее заполненными данными и признаками ошибок.
   include('form.php');
 }
-// Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
-else {
-  // Проверяем ошибки.
-  $errors = FALSE;
-  if (empty($_POST['fio'])) {
-    // Выдаем куку на день с флажком об ошибке в поле fio.
-    setcookie('fio_error', '1', time() + 24 * 60 * 60);
+else{
+    $regex_name="/[a-z,A-Z,а-я,А-Я,-]*$/";
+    $regex_email="/[a-z]+\w*@[a-z]+\.[a-z]{2,4}$/";
+    $errors = FALSE;
+    if (empty($_POST['name']) or !preg_match($regex_name,$_POST['name'])) {
+    print('Заполните имя.<br/>');
     $errors = TRUE;
-  }
-  else {
-    // Сохраняем ранее введенное в форму значение на месяц.
-    setcookie('fio_value', $_POST['fio'], time() + 30 * 24 * 60 * 60);
-  }
-
-// *************
-// TODO: тут необходимо проверить правильность заполнения всех остальных полей.
-// Сохранить в Cookie признаки ошибок и значения полей.
-// *************
-
-  if ($errors) {
-    // При наличии ошибок перезагружаем страницу и завершаем работу скрипта.
-    header('Location: index.php');
+    }
+    if (empty($_POST['email']) or !preg_match($regex_name,$_POST['email'])){
+    print('Заполните почту.<br/>');
+    $errors = TRUE;
+    }
+    if ($_POST['year']=='Выбрать'){
+    print('Выберите год рождения.<br/>');
+    $errors = TRUE;
+    }
+    if (empty($_POST['gender'])){
+    print('Выберите пол.<br/>');
+    $errors = TRUE;
+    }
+    if (empty($_POST['limb'])){
+    print('Выберите сколько у вас конечностей.<br/>');
+    $errors = TRUE;
+    }
+    if(!isset($_POST['power'])){
+        print('Выберите хотя бы одну суперспособность.<br/>');
+        $errors=TRUE;
+    }
+    if (empty($_POST['checkin'])){
+      print('Чек.<br/>');
+    $errors = TRUE;
+    }
+    if ($errors) {
+    print_r('Исправьте ошибки');
     exit();
+    }
+
+    $user = 'u52818';
+    $pass = '1096859';
+    $db = new PDO('mysql:host=localhost;dbname=u52819', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+
+    try {
+    $stmt = $db->prepare("INSERT INTO tabl SET name=?,email=?,year=?,gender=?,limb=?,bio=?");
+    $stmt -> execute(array($_POST['name'],$_POST['email'],$_POST['year'],$_POST['gender'],$_POST['limb'],$_POST['bio']));
+    $id=$db->lastInsertId();
+    $som=$db->prepare("INSERT INTO power SET id_power=:power,id_person=:person");
+    $som->bindParam(':person', $id);
+    foreach($_POST['power']  as $power){
+    $som->bindParam(':power', $power);
+    if($som->execute()==false){
+      print_r($som->errorCode());
+      print_r($som->errorInfo());
+      exit();
+    }
   }
-  else {
-    // Удаляем Cookies с признаками ошибок.
-    setcookie('fio_error', '', 100000);
-    // TODO: тут необходимо удалить остальные Cookies.
-  }
-
-  // Сохранение в БД.
-  // ...
-
-  // Сохраняем куку с признаком успешного сохранения.
-  setcookie('save', '1');
-
-  // Делаем перенаправление.
-  header('Location: index.php');
+    }
+    catch(PDOException $e){
+    print('Error : ' . $e->getMessage());
+    exit();
+    }
+    header('Location: ?save=1');
 }
-
+?>
