c09be258166a65810f7848ab366adf9545a5987b
tigr322 <70834080+tigr322@users.noreply.github.com>
2023-05-18 22:30:25 +0300

    Update index.php

--- Thirdback/index.php
+++ Thirdback/index.php
@@ -1,89 +1,71 @@
 <?php
-// Отправляем браузеру правильную кодировку,
-// файл index.php должен быть в кодировке UTF-8 без BOM.
 header('Content-Type: text/html; charset=UTF-8');
 
-// В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
-// и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-  // В суперглобальном массиве $_GET PHP хранит все параметры, переданные в текущем запросе через URL.
   if (!empty($_GET['save'])) {
-    // Если есть параметр save, то выводим сообщение пользователю.
-    print('Спасибо, результаты сохранены.');
   }
-  // Включаем содержимое файла form.php.
   include('form.php');
-  // Завершаем работу скрипта.
-  exit();
 }
-// Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
+else{
+    $regex_name="/[a-z,A-Z,а-я,А-Я,-]*$/";
+    $regex_email="/[a-z]+\w*@[a-z]+\.[a-z]{2,4}$/";
+    $errors = FALSE;
+    if (empty($_POST['name']) or !preg_match($regex_name,$_POST['name'])) {
+    print('Заполните имя.<br/>');
+    $errors = TRUE;
+    }
+    if (empty($_POST['email']) or !preg_match($regex_name,$_POST['email'])){
+    print('Заполните почту.<br/>');
+    $errors = TRUE;
+    }
+    if ($_POST['year']=='Выбрать'){
+    print('Выберите год рождения.<br/>');
+    $errors = TRUE;
+    }
+    if (empty($_POST['gender'])){
+    print('Выберите пол.<br/>');
+    $errors = TRUE;
+    }
+    if (empty($_POST['limb'])){
+    print('Выберите сколько у вас конечностей.<br/>');
+    $errors = TRUE;
+    }
+    if(!isset($_POST['power'])){
+        print('Выберите хотя бы одну суперспособность.<br/>');
+        $errors=TRUE;
+    }
+    if (empty($_POST['checkin'])){
+      print('Чек.<br/>');
+    $errors = TRUE;
+    }
+    if ($errors) {
+    print_r('Исправьте ошибки');
+    exit();
+    }
 
-// Проверяем ошибки.
-$errors = FALSE;
-if (empty($_POST['fio'])) {
-  print('Заполните имя.<br/>');
-  $errors = TRUE;
+    $user = 'u52818';
+    $pass = '1096859';
+    $db = new PDO('mysql:host=localhost;dbname=u52818', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+
+    try {
+    $stmt = $db->prepare("INSERT INTO tabl SET name=?,email=?,year=?,gender=?,limb=?,bio=?");
+    $stmt -> execute(array($_POST['name'],$_POST['email'],$_POST['year'],$_POST['gender'],$_POST['limb'],$_POST['bio']));
+    $id=$db->lastInsertId();
+    $som=$db->prepare("INSERT INTO power SET id_power=:power,id_person=:person");
+    $som->bindParam(':person', $id);
+    foreach($_POST['power']  as $power){
+    $som->bindParam(':power', $power);
+    if($som->execute()==false){
+      print_r($som->errorCode());
+      print_r($som->errorInfo());
+      exit();
+    }
+  }
+    }
+    catch(PDOException $e){
+    print('Error : ' . $e->getMessage());
+    exit();
+    }
+    header('Location: ?save=1');
 }
-
-if (empty($_POST['year']) || !is_numeric($_POST['year']) || !preg_match('/^\d+$/', $_POST['year'])) {
-  print('Заполните год.<br/>');
-  $errors = TRUE;
-}
-if (empty($_POST['email'])) {
-  print('Заполните email.<br/>');
-  $errors = TRUE;
-}
-
-
-
-// *************
-// Тут необходимо проверить правильность заполнения всех остальных полей.
-// *************
-
-if ($errors) {
-  // При наличии ошибок завершаем работу скрипта.
-  exit();
-}
-
-// Сохранение в базу данных.
-
-$user = 'u52818'; // Заменить на ваш логин uXXXXX
-$pass = '1096859'; // Заменить на пароль, такой же, как от SSH
-$db = new PDO('mysql:host=localhost;dbname=u52818', $user, $pass,
-  [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); // Заменить test на имя БД, совпадает с логином uXXXXX
-
-// Подготовленный запрос. Не именованные метки.
-try {
-  $stmt = $db->prepare("INSERT INTO application SET name = ?");
-  $stmt->execute([$_POST['fio']]);
-  $stmt = $db->prepare("INSERT INTO application SET email = ?");
-  $stmt->execute([$_POST['email']]);
-}
-catch(PDOException $e){
-  print('Error : ' . $e->getMessage());
-  exit();
-}
-
-
-
-//  stmt - это "дескриптор состояния".
- 
-//  Именованные метки.
-//$stmt = $db->prepare("INSERT INTO test (label,color) VALUES (:label,:color)");
-//$stmt -> execute(['label'=>'perfect', 'color'=>'green']);
- 
-//Еще вариант
-/*$stmt = $db->prepare("INSERT INTO users (firstname, lastname, email) VALUES (:firstname, :lastname, :email)");
-$stmt->bindParam(':firstname', $firstname);
-$stmt->bindParam(':lastname', $lastname);
-$stmt->bindParam(':email', $email);
-$firstname = "John";
-$lastname = "Smith";
-$email = "john@test.com";
-$stmt->execute();
-*/
-
-// Делаем перенаправление.
-// Если запись не сохраняется, но ошибок не видно, то можно закомментировать эту строку чтобы увидеть ошибку.
-// Если ошибок при этом не видно, то необходимо настроить параметр display_errors для PHP.
-header('Location: ?save=1');
+?>
